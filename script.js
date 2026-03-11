e76756d75b697aef0139ff8c4da35b3e307b6292
Ax Designer <alixf001@gmail.com>
2026-03-11 02:12:30 -0300

    Update script.js
    Signed-off-by: Ax Designer <alixf001@gmail.com>

--- script.js
+++ script.js
@@ -360,7 +360,7 @@ document.addEventListener('DOMContentLoaded', () => {
     if(missionBtn) {
         missionBtn.addEventListener('click', () => {
             alert("SISTEMA ONLINE: Nosso consultor ira lhe atender!");
-            const numeroWhatsApp = "55519937988445"; 
+            const numeroWhatsApp = "5551937988445"; 
             const linkWhatsApp = `https://wa.me/${numeroWhatsApp}`;
             window.open(linkWhatsApp, "_blank");
         });
